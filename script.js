110c2bbcb4a8b0c896de65795c183541282c6ad2
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-19 14:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function mapLetters(str){
+	str=str.toLowerCase();
+	let lettermap={};
+	
+	for(let i=0; i<str.length; i++){
+		let letter=str[i];
+		if(!lettermap[letter]){
+			lettermap[letter]=[];
+		}
+			lettermap[letter].push(i);
+		
+	}
+	
+	return lettermap;
+}
+
+
+let value=prompt("enter a value");
+alert(JSON.stringify(mapLetters(value)));
